126e043bed9e1c3aa5e669c0ae7bf0f6f9e15939
Lobotx <104751530+Lobotx@users.noreply.github.com>
2022-08-24 14:10:10 -0300

    Add files via upload

---
+++ nuevo/Ejercicios/Ejercicio clase 2 js.js
@@ -0,0 +1,16 @@
+let miNombre = "Joel";
+let miEdad = 24;
+let desarrollador = false;
+const fechaNacimiento = new Date("25 01 1998")
+const libroFav = 
+{
+    Nombre: "El club de los psicopatas",
+    Autor: "John Katzenbach",
+    fecha: "27 10 2021"
+}
+
+console.log(libroFav.fecha)
+console.log(miNombre);
+console.log(miEdad);
+console.log(desarrollador);
+console.log(fechaNacimiento);
